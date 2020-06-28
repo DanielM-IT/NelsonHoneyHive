678eba7e97a59af6af506ad0d6189afaa522f140
DanielM-IT <7ebed7@gmail.com>
2020-06-28 14:22:54 +1200

    Price calculator implemented and working

--- client/src/components/account/PriceCalculator.js
+++ client/src/components/account/PriceCalculator.js
@@ -2,39 +2,67 @@ import React, { useState } from 'react'
 
 
 const initialState = {
-    raw: '1',
-    pasturised: '.5',
-    creamed: '.5',
-    liquid: '1',
-    combHoney: '1.5',
-    clover: '1.5',
-    manuka: '2',
-    other: '1',
-    weight: '',
+    weight: 0,
     givenType: 'clover',
+    typePrice: 0,
     givenTexture: 'creamed',
-    isRaw: false
+    texturePrice: 0,
+    isRawPrice: 0,
+    isRaw: false,
+    recommendedReserve: '',
+    recommendedStart: ''
 }
 
 const PriceCalculator = () => {
 
     const [calculatorData, setData] = useState(initialState)
+    const [recommendedReserve, setRecReserve] = useState([])
+    const [recommendedStart, setRecStart] = useState([])
 
-    const {
-        raw,
-        pasturised,
-        creamed,
-        liquid,
-        combHoney,
-        clover,
-        manuka,
-        other,
+    let {
         weight,
         givenType,
+        typePrice,
         givenTexture,
-        isRaw
+        texturePrice,
+        isRawPrice,
+        isRaw,
     } = calculatorData
-    console.log(calculatorData)
+
+    function honeyType(givenType) {
+        switch (givenType) {
+            case 'clover':
+                return typePrice = typePrice + 1.50
+            case 'manuka':
+                return typePrice = typePrice + 2.00
+            case 'other':
+                return typePrice = typePrice + 1.00
+            default:
+                return null
+        }
+    }
+    function honeyTexture(givenTexture) {
+        switch (givenTexture) {
+            case 'creamed':
+                return texturePrice = texturePrice + .50
+            case 'liquid':
+                return texturePrice = texturePrice + 1.00
+            case 'comb':
+                return texturePrice = texturePrice + 1.50
+            default:
+                return null
+        }
+    }
+    function honeyRaw(isRaw) {
+        switch (isRaw) {
+            case true:
+                return isRawPrice = isRawPrice + 1.00
+            case false:
+                return isRawPrice = isRawPrice + .50
+            default:
+                return null
+        }
+    }
 
     const handleCheckBoxClick = e => {
         setData((prevState) => {
@@ -50,21 +78,25 @@ const PriceCalculator = () => {
 
     const onSubmit = async e => {
         e.preventDefault()
+        honeyType(givenType)
+        honeyTexture(givenTexture)
+        honeyRaw(isRaw)
 
-
-
-
-
-
-
-
+        const price = typePrice + texturePrice + isRawPrice
+        const reserveTotal = (Math.round(((weight / 100) * price) * 100) / 100).toFixed(2)
+        const startTotal = (Math.round((reserveTotal - (reserveTotal * .15)) * 100) / 100).toFixed(2)
+        console.log(startTotal)
+        setRecStart([])
+        setRecReserve([])
+        setRecStart(recommendedStart => [...recommendedStart, startTotal])
+        setRecReserve(recommendedReserve => [...recommendedReserve, reserveTotal])
     }
 
 
     return (
         <div className="preserve-whitespace">
             <h3 className="medium text-primary">Calculate Recommended Price</h3>
-            <form className="form" >
+            <form className="form" onSubmit={onSubmit}>
                 <div className="form-group">
                     <input
                         type="text"
@@ -73,7 +105,7 @@ const PriceCalculator = () => {
                         value={weight}
                         onChange={onChange}
                     />
-                    <small className="form-text">Provide the weight of your honey</small>
+                    <small className="form-text">Provide the weight of your honey in grams</small>
                 </div>
                 <div className="form-group">
                     <select
@@ -107,7 +139,15 @@ const PriceCalculator = () => {
                     <label>Raw  </label>
                     <input type="checkbox" id="isRaw" name="isRaw" value="isRaw" onChange={handleCheckBoxClick} />
                 </div>
-                <button type="submit" className="btn btn-primary my-1" onSubmit={onSubmit}>Calculate</button>
+                <div>
+                    <button type="submit" className="btn btn-primary my-1">Calculate</button>
+                </div>
+                <div>
+                    <h4 className="small text-primary"><br />Recommended Start Price:         ${recommendedStart}</h4>
+                </div>
+                <div>
+                    <h4 className="small text-primary">Recommended Reserve Price:   ${recommendedReserve}</h4>
+                </div>
             </form>
 
         </div>
