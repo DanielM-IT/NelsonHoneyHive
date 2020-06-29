e791662fffbe28986d486d61f4eb862eaf7fe32d
DanielM-IT <7ebed7@gmail.com>
2020-06-29 23:54:19 +1200

    Updated spinner background

--- client/src/components/layout/Spinner.js
+++ client/src/components/layout/Spinner.js
@@ -3,10 +3,12 @@ import spinner from '../../img/spinner.gif'
 
 export default () => (
     <Fragment>
-        <img
-            src={spinner}
-            style={{ width: '200px', margin: 'auto', display: 'block' }}
-            alt='Loading...'
-        />
+        <div className="spinner">
+            <img
+                src={spinner}
+                style={{ width: '200px', margin: 'auto', display: 'block' }}
+                alt='Loading...'
+            />
+        </div>
     </Fragment>
 )
